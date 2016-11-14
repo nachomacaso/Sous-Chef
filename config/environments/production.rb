eda8dd1402deacf416f37ac3076b93e7e943a565
nachomacaso <nathanmacaso@gmail.com>
2016-11-13 23:12:52 -0600

    Heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Added for Heroku
+  config.serve_static_files = true
+  config.serve_static_assets = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
