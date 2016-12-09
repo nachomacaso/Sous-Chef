09f20a35bc2d0ad97ceb7cacb3f05c85689fda6f
nachomacaso <nathanmacaso@gmail.com>
2016-12-09 14:46:23 -0600

    config.serve_static_files is deprecated and will be removed in Rails 5.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
   # Added for Heroku
-  config.serve_static_files = true
+  config.public_file_server.enabled = true
   config.serve_static_assets = true
 
   # Eager load code on boot. This eager loads most of Rails and
